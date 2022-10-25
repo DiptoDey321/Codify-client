b6aa3019585d15615ffe40574450d274c7939a71
Dipto Dey <diptodey2005@gmail.com>
2022-10-25 22:08:34 +0600

    footer section added

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-slate-500 py-10'>
+        <h3 className='text-white text-xl text-center'>All copy-write deserve by <span className='text-cyan-300'>Codify</span></h3>
+    </div>
+  )
+}
+
+export default Footer
